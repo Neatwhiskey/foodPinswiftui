de91aab9dfcd14003babfda61545119b837b4852
Neatwhiskey <44437144+Neatwhiskey@users.noreply.github.com>
2024-09-25 12:31:42 +0100

    Detail view enhancement

--- foodPin/foodPinApp.swift
+++ foodPin/foodPinApp.swift
@@ -9,6 +9,19 @@ import SwiftUI
 
 @main
 struct foodPinApp: App {
+    
+    init(){
+        let navBarAppearance = UINavigationBarAppearance ()
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.systemRed, .font: UIFont(name:"ArialRoundedMTBold", size: 35)!]
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.systemRed, .font: UIFont (name:"ArialRoundedMTBold", size: 28)!]
+        navBarAppearance.backgroundColor = .clear
+        navBarAppearance.backgroundEffect = .none
+        navBarAppearance.shadowColor = .clear
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+        UINavigationBar.appearance().compactAppearance = navBarAppearance
+                                                                           
+    }
     var body: some Scene {
         WindowGroup {
             RestaurantListView()
